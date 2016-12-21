6c8bd612f85b2612fcaf148dccb6d4eb5a6ba86c
BluePrinceEB <primeworld010@mail.ru>
2016-12-21 18:23:21 +0500

    Update Talon.lua

--- Talon.lua
+++ Talon.lua
@@ -1,7 +1,7 @@
 if myHero.charName ~= "Talon" then return end
 
-local LoLVer = "6.23.0.0"
-local ScrVer = 2
+local LoLVer = "6.24.0.0"
+local ScrVer = 3
 
 local function Talon_Update(data)
     if tonumber(data) > ScrVer then
