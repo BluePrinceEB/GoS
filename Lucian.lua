06c34f27cf4bce6d8fe0e157fc82e4d401e64af7
BluePrinceEB <primeworld010@mail.ru>
2017-04-19 19:36:05 +0500

    Update Lucian.lua

--- Lucian.lua
+++ Lucian.lua
@@ -7,7 +7,7 @@ local Alert = function(text, name)
         print("<b><font color=\"#ffb10a\">"..name.."- <font color=\"#ffffff\"><b>"..text) 
 end
 
-local LoLVer, ScrVer = "7.7", 3 
+local LoLVer, ScrVer = "7.8", 3 
 local AutoUpdate = function(data)     
         if tonumber(data) > ScrVer then         
 	        Alert("New version found! -" .. data)         
